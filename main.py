6f88e9ddfa4d9e3d0acffb27d4ca8500a59f1921
Cristian Ricardo <cristianricardo87@gmail.com>
2024-06-26 17:44:28 -0500

    session en Flask

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['TODO 1', 'TODO 2', 'TODO 3']
 
@@ -10,12 +12,12 @@ todos = ['TODO 1', 'TODO 2', 'TODO 3']
 def index():
     user_ip = request.remote_addr
     reponse = make_response(redirect('/hello'))
-    reponse.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
     return reponse
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'todos': todos 
