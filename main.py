47ddc3f5e67798d4eee82a9bea45ca2de8f53193
Cristian Ricardo <cristianricardo87@gmail.com>
2024-06-26 17:09:36 -0500

    Flask Bootstrap

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 todos = ['TODO 1', 'TODO 2', 'TODO 3']
-
 
 @app.route('/')
 def index():
@@ -20,3 +21,8 @@ def hello():
         'todos': todos 
     }
     return render_template('hello.html', **context)
+
+# Errors
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
